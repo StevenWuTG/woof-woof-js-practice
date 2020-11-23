730593eae242fd6537eae39d42f65b0443b2a2fd
StevenWuTG <stevenwu91@icloud.com>
2020-11-23 11:20:08 -0500

    deliverable 2 done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const dogBar = document.querySelector("#dog-bar")
 const dogContainer = document.querySelector("#dog-summary-container")
 
+
 function loadPups(){
     fetch('http://localhost:3000/pups')
     .then(response => response.json())
@@ -14,8 +15,6 @@ function loadPups(){
 }
 
 function renderPup(pup){
-    
-    
         const pupSpan = document.createElement("span")
         
         pupSpan.innerText = pup.name
@@ -23,9 +22,36 @@ function renderPup(pup){
 
         // debugger
         dogBar.append(pupSpan)
-
-
+        pupSpan.addEventListener("click", function(){
+            showDogCard(pup)
+        })
 }
+
+
+function showDogCard(pup){
+    const card = document.createElement("div")
+    card.setAttribute("id", "dog-info")
+    const img = document.createElement("img")
+    img.src = pup.image
+    const h2 = document.createElement("h2")
+    h2.textContent = pup.name
+    
+    card.append(img, h2)
+    dogContainer.append(card)
+}
+
+
+// event listeners 
+
+dogBar.addEventListener("click", function(e) {
+
+    if (e.target.matches("span")){
+        const card = document.createElement("div")
+        card.setAttribute("id", "dog-card")
+
+    }
+})
+
 
 
 loadPups()
